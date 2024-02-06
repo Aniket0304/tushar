3b5e3809c73b4b4092a76159bde38cc8ec96e68f
aniket <dubeyaniket7839@gmail.com>
2024-02-06 16:53:31 +0530

    new

--- standard_template_library.cpp
+++ standard_template_library.cpp
@@ -6,6 +6,10 @@ int main(){
     array <int , 4> a = {1,2,3,4};// ye stl ka main implimentation code hai.
     int size = a.size();
     for (int i =0;i<size;i++){
-        cout << a[i] <<" ";
+        cout << a[i] <<" "<<endl;
     }
+   cout << "Element at second index is "<< a.at(2)<<endl;
+   cout << "Empty or not "<< a.empty()<<endl;
+   cout << "First element "<< a.front()<<endl; //ye sare stl ke code hai jo array wali library me aate hai.
+   cout << "last element "<< a.back();
 }
